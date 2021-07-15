3b531f1edfbcf941b6f28d6b8c620e16e4e38e34
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2021-07-15 18:10:57 +0200

    Add react Speech and try it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Speech from 'react-speech';
 
 const App = () => {
-  return <div className="App">testing-read-more-react</div>
+  return <div className="App">
+    <h1>testing-react-speech</h1>
+    <Speech
+      text="I have the default settings" />
+    <Speech
+      text="I have altered my voice"
+      voice="Google UK English Female" /></div>
 }
 
 export default App;
