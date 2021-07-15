65cd2be4a71111bfaf61c030022fd5ad9d0d6173
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2021-07-15 18:43:21 +0200

    Add random style

--- src/App.js
+++ src/App.js
@@ -57,37 +57,61 @@ const buttonStyle = {
 const App = () => {
   return <div className="App">
     <h1>testing-react-speech</h1>
-    <Speech
-      text="I have the default settings" />
-    <Speech
-      text="I have altered my voice"
-      voice="Google UK English Female" />
-    <Speech
-      // styles={buttonStyle}
-      text="I have changed the colour of the play button and made it smaller" />
-    <Speech
-      text="I have altered the pitch, rate and volume of my voice"
-      pitch="0.5"
-      rate="0.5"
-      volume="0.1"
-      lang="en-GB"
-      voice="Daniel" />
-    <Speech
-      text="I have all properties set to their default"
-      pitch="1"
-      rate="1"
-      volume="1"
-      lang="en-GB"
-      voice="Google UK English Male" />
-    <Speech
-      stop={true}
-      pause={true}
-      resume={true}
-      text="I am displaying all buttons" />
-    <Speech
-      textAsButton={true}
-      displayText="Hello"
-      text="I have text displayed as a button" /></div>
+
+    <div className="speeches-container">
+      <div className="speech-box">
+        <Speech
+          text="I have the default settings" />
+      </div>
+
+      <div className="speech-box">
+        <Speech
+          text="I have altered my voice"
+          voice="Google UK English Female" />
+      </div>
+
+      <div className="speech-box">
+        <Speech
+          // styles={buttonStyle}
+          text="I have changed the colour of the play button and made it smaller" />
+      </div>
+
+      <div className="speech-box">
+        <Speech
+          text="I have altered the pitch, rate and volume of my voice"
+          pitch="0.5"
+          rate="0.5"
+          volume="0.1"
+          lang="en-GB"
+          voice="Daniel" />
+      </div>
+
+      <div className="speech-box">
+        <Speech
+          text="I have all properties set to their default"
+          pitch="1"
+          rate="1"
+          volume="1"
+          lang="en-GB"
+          voice="Google UK English Male" />
+      </div>
+
+      <div className="speech-box">
+        <Speech
+          stop={true}
+          pause={true}
+          resume={true}
+          text="I am displaying all buttons" />
+      </div>
+
+      <div className="speech-box">
+        <Speech
+          textAsButton={true}
+          displayText="Hello"
+          text="I have text displayed as a button" />
+      </div>
+    </div>
+  </div>
 }
 
 export default App;
